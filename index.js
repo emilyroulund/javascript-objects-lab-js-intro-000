c0d8bfdfff29a21bee890a1d0b2f7777ff82c5de
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-19 22:49:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var recipes = {}; 
 
-updateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value; 
+  return object; 
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   
 }
 
-destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+function deleteFromObjectByKey(object, key){
   
 }
 
-deleteFromObjectByKey(object, key)
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
